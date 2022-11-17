e274733a86049ffde76facd4eada651c326765e7
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-11-16 21:22:32 -0300

    feat: implementa caso de uso GetBalanceValueByUserId com DbGetBalanceValueByUserId

---
+++ src/data/usecases/account/db-get-balance-value-by-user-id.ts
@@ -0,0 +1,15 @@
+import { GetBalanceValueByUserIdRepository } from '@/data/protocols/database/account/get-balance-value-by-user-id'
+import { GetBalanceValueByUserId } from '@/domain/usecases/account'
+
+export class DbGetBalanceValueByUserId implements GetBalanceValueByUserId {
+  constructor(
+    private readonly getBalanceValueByUserIdRepository: GetBalanceValueByUserIdRepository
+  ) {}
+  async get(
+    params: GetBalanceValueByUserId.Params
+  ): Promise<GetBalanceValueByUserId.Result> {
+    return await this.getBalanceValueByUserIdRepository.getByUserId(
+      params.userId
+    )
+  }
+}
