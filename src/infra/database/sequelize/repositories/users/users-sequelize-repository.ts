480b143397663fc805b89f1e38c57074ac05e6a1
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-11-20 11:03:47 -0300

    feat: implementa GetUserByUsernameRepository com UsersSequelizeRepository

--- src/infra/database/sequelize/repositories/users/users-sequelize-repository.ts
+++ src/infra/database/sequelize/repositories/users/users-sequelize-repository.ts
@@ -1,22 +1,45 @@
-import { AddUserRepository } from "@/data/protocols/database/user/add-user-repository";
-import { GetUserByIdRepository } from "@/data/protocols/database/user/get-user-by-id-repository";
-import { AddUser } from "@/domain/usecases/user";
+import { AddUserRepository } from '@/data/protocols/database/user/add-user-repository'
+import { GetUserByIdRepository } from '@/data/protocols/database/user/get-user-by-id-repository'
+import { GetUserByUsernameRepository } from '@/data/protocols/database/user/get-user-by-username-repository'
+import { AddUser } from '@/domain/usecases/user'
 import UsersModel from '../../models/users-model'
-export class UsersSequelizeRepository implements AddUserRepository, GetUserByIdRepository {
-  async add (userData: AddUser.Params): Promise<AddUser.Result>{
+export class UsersSequelizeRepository
+  implements
+    AddUserRepository,
+    GetUserByIdRepository,
+    GetUserByUsernameRepository
+{
+  async add(userData: AddUser.Params): Promise<AddUser.Result> {
     const user = await UsersModel.create(userData)
 
     return user.toJSON()
-  };
+  }
 
-  async getById (id: GetUserByIdRepository.Params): Promise<GetUserByIdRepository.Result> {
+  async getById(
+    id: GetUserByIdRepository.Params
+  ): Promise<GetUserByIdRepository.Result> {
     const user = await UsersModel.findOne({
       where: { id },
       include: {
-        all: true
-      }
+        all: true,
+      },
     })
 
-    return user?.toJSON() as any || null
+    return (user?.toJSON() as any) || null
+  }
+
+  async getByUsername(
+    username: GetUserByUsernameRepository.Params
+  ): Promise<GetUserByUsernameRepository.Result> {
+    const user = await UsersModel.findOne({
+      where: {
+        username
+      },
+      include: {
+        all: true,
+      },
+    })
+
+    return (user?.toJSON() as any) || null
   }
 }
