fba0fdedfe2306d0512df8031e81d694919bdc59
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-11-20 11:00:24 -0300

    feat: implementa GetUserByIdRepository na UsersSequelizeRepository

--- src/infra/database/sequelize/repositories/users/users-sequelize-repository.ts
+++ src/infra/database/sequelize/repositories/users/users-sequelize-repository.ts
@@ -1,10 +1,22 @@
 import { AddUserRepository } from "@/data/protocols/database/user/add-user-repository";
+import { GetUserByIdRepository } from "@/data/protocols/database/user/get-user-by-id-repository";
 import { AddUser } from "@/domain/usecases/user";
 import UsersModel from '../../models/users-model'
-export class UsersSequelizeRepository implements AddUserRepository {
+export class UsersSequelizeRepository implements AddUserRepository, GetUserByIdRepository {
   async add (userData: AddUser.Params): Promise<AddUser.Result>{
     const user = await UsersModel.create(userData)
 
     return user.toJSON()
   };
+
+  async getById (id: GetUserByIdRepository.Params): Promise<GetUserByIdRepository.Result> {
+    const user = await UsersModel.findOne({
+      where: { id },
+      include: {
+        all: true
+      }
+    })
+
+    return user?.toJSON() as any || null
+  }
 }
